148cc426280819bcd93cd11815eda8dc78c5a4d2
Andreas Kluth <mail@andreaskluth.net>
2013-05-13 23:46:37 +0200

    Broken build due to files ignored by tortoise.

---
+++ UsingMassTransitWithRabbitMQ/UsingMassTransitWithRabbitMQ.Consumer/SystemStatus/SystemStatusConsumer.cs
@@ -0,0 +1,16 @@
+﻿using MassTransit;
+using Samples.MassTransit.Messages;
+using log4net;
+
+namespace Samples.MassTransit.SystemStatus
+{
+  public class SystemStatusConsumer : Consumes<ISystemStatusMessage>.All
+  {
+    private readonly ILog m_Log = LogManager.GetLogger( typeof( SystemStatusConsumer ) );
+
+    public void Consume( ISystemStatusMessage message )
+    {
+      m_Log.InfoFormat( "Message Received: MemoryLoad {0}, CpuLoad {1}", message.MemoryLoad, message.CpuLoad );
+    }
+  }
+}
---
+++ UsingMassTransitWithRabbitMQ/UsingMassTransitWithRabbitMQ/ProducerInstaller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using MassTransit;
+using MassTransit.Log4NetIntegration;
+using Samples.MassTransit.SystemStatus;
+
+namespace Samples.MassTransit
+{
+  public class ProducerInstaller : IWindsorInstaller
+  {
+    public void Install( IWindsorContainer container, IConfigurationStore store )
+    {
+      container.Register( AllTypes.FromThisAssembly().BasedOn<IConsumer>() );
+
+      Func<IServiceBus> serviceBusFactoryMethod = () => ServiceBusFactory.New(
+        sbc =>
+        {
+          sbc.UseLog4Net();
+          sbc.UseRabbitMq();
+          sbc.ReceiveFrom( "rabbitmq://localhost/sample_producer" );
+          sbc.Subscribe( x => x.LoadFrom( container ) );
+          sbc.UseControlBus();
+          sbc.Validate();
+        } );
+      container.Register( Component.For<IServiceBus>().UsingFactoryMethod( serviceBusFactoryMethod ) );
+
+      Func<PerformanceCounter> processorCounterFactoryMethod = () => new PerformanceCounter( "Process", "% Processor Time", "_Total" );
+      container.Register( Component.For<PerformanceCounter>().UsingFactoryMethod( processorCounterFactoryMethod ).Named( "Processor" ) );
+
+      Func<PerformanceCounter> memoryCounterFactoryMethod = () => new PerformanceCounter( "Memory", "Available MBytes" );
+      container.Register( Component.For<PerformanceCounter>().UsingFactoryMethod( memoryCounterFactoryMethod ).Named( "Memory" ) );
+
+      container.Register(
+        Component
+          .For<SystemStatusController>()
+          .ImplementedBy<SystemStatusController>()
+          .DependsOn(
+            Dependency.OnComponent( "ramPerformanceCounter", "Memory" ),
+            Dependency.OnComponent( "cpuPerformanceCounter", "Processor" ) ) );
+    }
+  }
+}
